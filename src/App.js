ee42940fb82b6ede4b703f1271f5467c233d151d
LindaPaloma <lindishernandez@gmail.com>
2022-07-26 12:55:07 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Nueva App <code>src/App.js</code> con cambios de contenido.
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Linda Paloma Hernandez
         </a>
       </header>
     </div>
