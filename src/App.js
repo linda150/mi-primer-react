4384a44c24860674f9639e161f481f7e5c7f9392
LindaPaloma <lindishernandez@gmail.com>
2022-08-05 13:13:44 -0500

    sumatoria de arraycontador

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
-import logo from "./logo.svg";
 import "./App.css";
+import Contador from "./Contador.js";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Nueva App <code>src/App.js</code> con cambios de contenido.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Linda Paloma Hernandez
-        </a>
+        <p>Linda Paloma Hernandez</p>
+        <Contador />
       </header>
     </div>
   );
